425dd205f77e43925cb96df39b122d22ef9cd6b2
elnormous <elviss@elviss.lv>
2024-05-24 12:28:51 +0300

    Add parse function

--- include/gettextpp.hpp
+++ include/gettextpp.hpp
@@ -1,9 +1,21 @@
 #ifndef GETTEXTPP_HPP
 #define GETTEXTPP_HPP
 
+#include <map>
+#include <string>
+
 namespace gettextpp
 {
-    
+	template <class Iterator>
+	std::map <std::string, std::string> parse(const Iterator begin, Iterator end)
+	{
+		return {};
+	}
+
+	std::map <std::string, std::string> parse(const std::string& s)
+	{
+		return parse(s.begin(), s.end());
+	}
 }
 
 #endif
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,1 +1,17 @@
+#include <string>
 #include "doctest/doctest.h"
+#include "gettextpp.hpp"
+
+TEST_CASE("Parse empty string")
+{
+	std::string empty;
+	const auto result = gettextpp::parse(empty);
+	REQUIRE(result.empty());
+}
+
+TEST_CASE("Parse empty iterators")
+{
+	std::string empty;
+	const auto result = gettextpp::parse(empty.begin(), empty.end());
+	REQUIRE(result.empty());
+}
